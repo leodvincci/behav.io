0bce34ca2fb1fbb1c71695e3d306e68340773d42
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-03 18:13:36 -0500

    Updated urls.py, added view function registration

---
+++ behavio_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("/api/v1/registration",views.registration, name="registration")
+]
--- behavio_app/views.py
+++ behavio_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def registration(request):
+    return None
