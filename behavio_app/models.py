3f6c5dc8bf42fc7b9b53e909f7c17b0f6121c9b5
bridgesMichael <bridges.michael.w@gmail.com>
2023-05-03 13:18:41 -0700

    migrated models to db called behavio, added app to settings

--- behavio_app/models.py
+++ behavio_app/models.py
@@ -1,20 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(AbstractUser):
-    email           = models.EmailField(unique=True)
-    first_name      = models.CharField(max_length=255)
-    last_name       = models.CharField(max_length=255)
-    USERNAME_FIELD  = "email"
-    REQUIRED_FIELDS = []
-    
-    def __str__(self):
-        return f"{self.name} | {self.email}"
     
 class Category(models.Model):
-    category_txt    = models.CharField(max_length=255)
+    category_txt    = models.CharField(max_length=200)
     
     def __str__(self):
         return f"{self.category_text}"
@@ -27,14 +17,14 @@ class Question(models.Model):
         return f"{self.category} | {self.question_text}"
 
 class FavoritedQuestion(models.Model):
-    User            = models.ForeignKey(User, on_delete=models.CASCADE)
+    app_user        = models.ForeignKey(User, on_delete=models.CASCADE)
     Question        = models.ForeignKey(Question, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.user_id.name}: {self.question_id.question_text}"
 
 class Response(models.Model):
-    User            = models.ForeignKey(User, on_delete=models.CASCADE)
+    app_user        = models.ForeignKey(User, on_delete=models.CASCADE)
     Question        = models.ForeignKey(Question, on_delete=models.CASCADE)
     response_S      = models.TextField()
     response_T      = models.TextField()
