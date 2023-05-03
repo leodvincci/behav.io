53b4b5d88e7637405eb1cb929c2fb5523892c9d0
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-03 18:30:10 -0500

    Update: added included path to project urls, added json response to registration view

--- behavio_app/views.py
+++ behavio_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 # Create your views here.
 def registration(request):
-    return None
+    res = {"042": "Success"}
+    return JsonResponse(res)
