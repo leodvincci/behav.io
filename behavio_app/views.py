47bd6b267a28a63a4a1b83794d14314b8c431c44
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-03 18:36:53 -0500

    update: added django rest framework api view to registration view

--- behavio_app/views.py
+++ behavio_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
+@api_view(["POST"])
 def registration(request):
     res = {"042": "Success"}
     return JsonResponse(res)
