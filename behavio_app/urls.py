bf9c5ecf780feb070cb2dd9d35b7b3a0daa10f65
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-03 18:45:30 -0500

    Completed User Registration API

--- behavio_app/urls.py
+++ behavio_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/v1/registration/", views.registration, name="registration")
+    path("api/v1/registration", views.registration, name="registration")
 ]
--- behavio_app/views.py
+++ behavio_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 @api_view(["POST"])
 def registration(request):
-    res = {"042": "Success"}
+    first_name = request.data["first_name"]
+    last_name = request.data["last_name"]
+    email = request.data["email"]
+    password = request.data["password"]
+    app_user = User(first_name=first_name, last_name=last_name, email=email, password=password, username = email)
+    app_user.save()
+    print(app_user)
+    res = {"042": f"User:{email} Registration Was A Success!"}
     return JsonResponse(res)
