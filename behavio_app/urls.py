80c57a1c78f57420f54a0c730b1bcb14939f82e6
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-03 23:13:54 -0500

    Fix/Update: Fixed user registration save user object, created user login url and view. User login api complete

--- behavio_app/urls.py
+++ behavio_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/v1/registration", views.registration, name="registration")
+    path("api/v1/registration", views.registration, name="registration"),
+    path("api/v1/login", views.login, name="login"),
+    path("api/v1/logout", views.logout, name="logout"),
 ]
--- behavio_app/views.py
+++ behavio_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
@@ -11,8 +12,26 @@ def registration(request):
     last_name = request.data["last_name"]
     email = request.data["email"]
     password = request.data["password"]
-    app_user = User(first_name=first_name, last_name=last_name, email=email, password=password, username = email)
+    app_user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password, username=email)
     app_user.save()
     print(app_user)
     res = {"042": f"User:{email} Registration Was A Success!"}
     return JsonResponse(res)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data["username"]
+    password = request.data["password"]
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        print(username,password)
+        print("User Authorized: ", user)
+        return JsonResponse({"user": username})
+    else:
+        return HttpResponse("<h1>Unauthorized User</h1>")
+
+
+@api_view(["GET"])
+def logout(request):
+    return None
