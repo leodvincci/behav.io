d171cba83d9467c56e8f8dac127e24c45455a36c
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-05-06 10:34:54 -0500

    added btn styling

--- behavio_frontend/src/App.tsx
+++ behavio_frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react"
+import { redirect } from "react-router-dom"
+
 const App = () => {
+
+  useEffect(() => {
+    redirect('/login')
+  }, [])
   return (
     <div className="bg-primary h-screen text-accent">
-      Behavio
-      <button className="btn">Button</button>
+      HomePage
+      <button className='btn text-secondary mx-auto tracking-widest bg-primary-light hover:bg-primary-dark shadow-lg hover:shadow-xl active:shadow-lg'>Button</button>
     </div>
   )
 }
