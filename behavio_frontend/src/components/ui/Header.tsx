cf2f0b92c3a05a90bc62cff3c225e74c7aa275e4
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-05-07 00:14:38 -0500

    fixed gap

--- behavio_frontend/src/components/ui/Header.tsx
+++ behavio_frontend/src/components/ui/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="flex flex-col md:flex-row items-center justify-between bg-primary border-b-[2px] border-b-secondary shadow-lg p-6">
       <Logo width={250}/>
-      <nav className="flex flex-col md:flex-row text-center lg:gap-4">
+      <nav className="flex flex-col md:flex-row text-center md:gap-4">
         <Link className="hover:text-secondary-dark hover:cursor-pointer" to="/profile-settings">Settings</Link>
         <Link className="hover:text-secondary-dark hover:cursor-pointer" to="/logout">Logout</Link>
       </nav>
