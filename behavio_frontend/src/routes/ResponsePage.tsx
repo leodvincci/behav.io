413e604ad7420366ac3bc4747f57f8b1d32a7412
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-05-08 22:21:20 -0500

    working

--- behavio_frontend/src/routes/ResponsePage.tsx
+++ behavio_frontend/src/routes/ResponsePage.tsx
@@ -9,7 +9,7 @@ const ResponsePage: React.FC = () => {
     <main className="min-h-fit py-20 bg-accent min-w-full flex flex-col items-center tracking-wide text-black gap-10 p-3">
       <section className="flex flex-col md:flex-row gap-10 md:gap-20 items-center">
         <SettingsImage width={250} />
-        <h1 className="text-4xl md:text-6xl text-offBlue">QUESTION</h1>
+        <h1 className="text-4xl md:text-6xl text-offBlue">Respond</h1>
       </section>
       <section className="w-full h-screen bg-accent max-w-6xl">
         {/* FORM FOR STAR METHOD INPUT */}
