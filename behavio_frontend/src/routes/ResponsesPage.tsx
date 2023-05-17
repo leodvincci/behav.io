a8ee3d9234c6caa1eb164f34f418caf3ecf1d84f
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-05-17 10:14:32 -0500

    new structure

--- behavio_frontend/src/routes/ResponsesPage.tsx
+++ behavio_frontend/src/routes/ResponsesPage.tsx
@@ -6,6 +6,7 @@ import { BsHeart, BsHeartFill } from 'react-icons/bs';
 import QuestionsImage from '../components/ui/QuestionsImage';
 import { ResponseType } from '../types/types';
 import { useState } from 'react';
+import FindingDataImage from '../components/ui/FindingDataImage';
 
 const ResponsesPage = () => {
   const data = useLoaderData(); // Loads the  data from the loader in main.jsx
@@ -13,6 +14,7 @@ const ResponsesPage = () => {
   const navigate = useNavigate();
   const [aiFeedback, setAiFeedback] = useState<string>('');
   const [grade, setGrade] = useState<string>('');
+  const [responseId, setResponseId] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleDeleteResponse = async (id: number) => {
@@ -40,7 +42,7 @@ const ResponsesPage = () => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        // `http://127.0.0.1:8000/api/v1/auto_feedback/${responseId}/`,
+        `http://127.0.0.1:8000/api/v1/auto_feedback/${responseId}/`,
         {
           credentials: 'include',
           method: 'POST',
@@ -63,6 +65,11 @@ const ResponsesPage = () => {
       console.log(error);
     }
   };
+
+  const handleResponseSelect = (e: any) => {
+    setResponseId(e.target.value);
+  };
+
   return (
     <>
       <Header />
@@ -76,25 +83,69 @@ const ResponsesPage = () => {
           </div>
         </section>
 
+        <section className="grid grid-cols-1 bg-primary-dark rounded-xl w-full p-10 text-center gap-10">
+          <div className="p-10 text-secondary rounded-xl bg-opacity-90 flex flex-col justify-around items-center tracking-widest w-full basis-1">
+            {!aiFeedback && !isLoading && (
+              <div className="flex flex-col justify-center items-center gap-5 shadow-xl p-10 bg-primary-light rounded-lg">
+                <h4 className="text-2xl text-accent">AI Feedback</h4>
+                <p>Select which response you want feedback on then submit</p>
+                <form onSubmit={() => handleAutoFeedback(responseId)}>
+                  <select
+                    className="select w-full max-w-xl bg-primary-dark"
+                    onChange={handleResponseSelect}
+                  >
+                    <option disabled selected>
+                      Select your response
+                    </option>
+                    {data.responses.map((response: ResponseType) => (
+                      <option key={response.id} value={response.id}>
+                        {response.question_text}
+                      </option>
+                    ))}
+                  </select>
+                  <button
+                    className="btn text-secondary w-full max-w-xl mt-3 mx-auto tracking-widest bg-primary-light hover:bg-primary-dark shadow-lg hover:shadow-xl active:shadow-lg"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            )}
+            {aiFeedback && !isLoading && (
+              <div className="flex flex-col gap-10">
+                <div className="flex flex-col justify-center items-center gap-5 border-2 border-secondary-light p-10">
+                  <h4 className="text-2xl text-accent">AI Feedback</h4>
+                  <p>{aiFeedback}</p>
+                  <p className="text-2xl text-accent">{grade}</p>
+                </div>
+                <button
+                  onClick={() => {
+                    setAiFeedback('');
+                    setGrade('');
+                  }}
+                  className="btn text-secondary w-full max-w-md mt-3 mx-auto tracking-widest bg-primary-light hover:bg-primary-dark shadow-lg hover:shadow-xl active:shadow-lg"
+                >
+                  Try another response
+                </button>
+              </div>
+            )}
+            {!aiFeedback && isLoading && (
+              <div className="flex flex-col justify-center items-center gap-5 border-2 border-secondary-light p-10">
+                <h4 className="text-2xl text-accent">Loading Feedback..</h4>
+                <FindingDataImage width={500} />
+                <p>Please be patient, this make take a few moments... </p>
+              </div>
+            )}
+          </div>
+        </section>
+
         <section className="grid grid-cols-1 bg-primary-dark rounded-xl w-full p-10 text-center lg:grid-cols-2 gap-10">
           {data.responses.map((response: ResponseType) => (
             <div
               key={response.id}
               className="p-10 bg-primary-light text-secondary uppercase rounded-xl bg-opacity-90 flex flex-col justify-around items-center tracking-widest w-full basis-1"
             >
-              {!aiFeedback && (
-                <div className="flex flex-col justify-center items-center gap-5 border-2 border-secondary-light">
-                  <h4 className="text-2xl text-accent">Loading Feedback..</h4>
-                  <p>Please be patient, this make take a few moments... </p>
-                </div>
-              )}
-              {aiFeedback && (
-                <div className="flex flex-col justify-center items-center gap-5 border-2 border-secondary-light p-10">
-                  <h4 className="text-2xl text-accent">AI Feedback</h4>
-                  <p>{aiFeedback}</p>
-                  <p className="text-2xl text-accent">{grade}</p>
-                </div>
-              )}
               <p className="text-offBlue max-w-lg my-10">
                 {response.question_text}
               </p>
@@ -136,6 +187,7 @@ const ResponsesPage = () => {
                 <button
                   className="btn text-secondary w-full mt-3 mx-auto tracking-widest bg-primary-light hover:bg-primary-dark shadow-lg hover:shadow-xl active:shadow-lg"
                   onClick={() => handleAutoFeedback(response.id)}
+                  disabled
                 >
                   Ai Feedback
                 </button>
@@ -153,68 +205,5 @@ const ResponsesPage = () => {
     </>
   );
 };
-
-// <div
-//   key={response.id}
-//   className="p-10 bg-primary-light text-secondary uppercase rounded-xl bg-opacity-90 flex flex-col justify-around items-center gap-32 tracking-widest w-full"
-// >
-//   {/* Call fetchQuestion to call the questions API and return its text */}
-//   <p className="text-offBlue max-w-lg">
-//     {response.question_text}
-//   </p>
-//   <div className="flex flex-col">
-//     <ul className="flex flex-col gap-4">
-//       <li className="card-text flex flex-col gap-1">
-//         <span className="font-bold">Situation</span>
-//         <span className="font-thin">
-//           {response.response_S || 'Error'}
-//         </span>
-//       </li>
-//       <li className="card-text flex flex-col gap-1">
-//         <span>Task</span>
-//         <span className="font-thin">
-//           {response.response_T || 'Error'}
-//         </span>
-//       </li>
-//       <li className="card-text flex flex-col gap-1">
-//         <span>Action</span>
-//         <span className="font-thin">
-//           {response.response_A || 'Error'}
-//         </span>
-//       </li>
-//       <li className="card-text flex flex-col gap-1">
-//         <span>Results</span>
-//         <span className="font-thin">
-//           {response.response_R || 'Error'}
-//         </span>
-//       </li>
-//     </ul>
-//   </div>
-//   <div className="flex flex-col md:flex-row gap-4 w-full max-w-xl">
-//     <button
-//       onClick={() => handleDeleteResponse(response.id)}
-//     >
-//       Delete
-//     </button>
-//     <button
-//       className="btn text-secondary w-full md:w-fit mt-3 mx-auto tracking-widest bg-primary-light hover:bg-primary-dark shadow-lg hover:shadow-xl active:shadow-lg"
-//       onClick={() => handleAutoFeedback(response)}
-//     >
-//       Ask Chad
-//     </button>
-//   </div>
-
-{
-  /* <aside>
-                      <div className="flex gap-1">
-                        <BsHeartFill className="text-red-500" />
-                        <p>{response.likes}</p>
-                      </div>
-                      <div className="flex gap-1">
-                        <BsHeart className="text-red-500" />
-                        <p>{response.dislikes}</p>
-                      </div>
-                    </aside> */
-}
 
 export default ResponsesPage;
